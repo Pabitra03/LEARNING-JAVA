112cfcbf892a9d9b8ecfa3a5b46f7ac59c27a320
Pabitra03 <pabitramahakur007@gmail.com>
2024-01-24 20:18:43 +0530

    pro15 added

---
+++ pro15.java
@@ -0,0 +1,12 @@
+public class pro15 {
+    public static void main(String[] args) {
+        int a = 15;
+        if(a>18){
+            System.out.println("You can drive");
+        }
+        else{
+            System.out.println("You cant drive");
+        }
+    }
+    
+}
