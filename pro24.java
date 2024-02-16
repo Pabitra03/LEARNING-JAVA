7a7da2885711263c74c6436d5108bc75b0a43ca7
Pabitra03 <pabitramahakur007@gmail.com>
2024-02-16 22:14:33 +0530

    new programs

---
+++ pro24.java
@@ -0,0 +1,10 @@
+public class pro24 {
+    public static void main(String[] args) {
+        int b = 0;
+        do{
+            System.out.println(b);
+            b++;
+        }
+        while(b<11);
+    }
+}
---
+++ pro25.java
@@ -0,0 +1,13 @@
+public class pro25 {
+    public static void main(String[] args) {
+        //for(int i = 1; i<=10; i++){
+            //System.out.println(i);
+        //}
+        int n = 10;
+        for(int i=1; i<=n; i++){
+            System.out.println("The even number is "+2*i);
+
+        }
+    }
+    
+}
