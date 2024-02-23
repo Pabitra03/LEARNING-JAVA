a989e8cf34d8af0de643e41cf9914bf6aa728d9e
Pabitra03 <pabitramahakur007@gmail.com>
2024-02-23 12:15:55 +0530

    39,40 pro added

--- pro38.java
+++ pro38.java
@@ -8,6 +8,24 @@ public class pro38 {
         marks[3] = 15;
         marks[4] = 16;
         System.out.println(marks[4]);
+
+
+        // Another way to create arrays
+        int arr[];
+        arr = new int[6];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        arr[3]=4;
+        arr[4]=5;
+        arr[5]=6;
+        System.out.println(arr[4]);
+
+
+        //last way
+        int arr2[] = {1,2,3,4,5,6};
+        System.out.println(arr2[2]);
+
     }
     
 }
---
+++ pro39.java
@@ -0,0 +1,7 @@
+public class pro39 {
+    public static void main(String[] args) {
+        int marks[] = {10,11,12,13,14,15};
+        System.out.println(marks.length);
+    }
+    
+}
