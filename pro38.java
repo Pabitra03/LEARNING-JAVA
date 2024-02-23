67d88c531dbdd5746a1336cc6dd8e44a18e1dc7d
Pabitra03 <pabitramahakur007@gmail.com>
2024-02-23 11:57:13 +0530

    Update pro38.java

--- pro38.java
+++ pro38.java
@@ -1,7 +1,13 @@
 public class pro38 {
     public static void main(String[] args) {
         //ARRAYS
-        
+        int marks[] = new int[5];
+        marks[0] = 12;
+        marks[1] = 13;
+        marks[2] = 14;
+        marks[3] = 15;
+        marks[4] = 16;
+        System.out.println(marks[4]);
     }
     
 }
